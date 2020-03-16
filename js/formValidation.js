1206fadba5bdf5f5d1c605a9c2330851380f6515
mgalkus <martynasgalkus@hotmail.com>
2020-03-16 17:55:04 +0200

    AJAX implemented, phone validation and radio buttons styling

--- js/formValidation.js
+++ js/formValidation.js
@@ -1,33 +1,67 @@
 const submitButton = document.querySelector(".submit");
 const textInputs = document.querySelectorAll(".js-text__input");
-const emailInput = document.querySelector(".js-email__input");
+const emailField = document.querySelector(".js-email__input");
+const phoneField = document.querySelector(".js-phone");
 const radios = document.querySelectorAll(".js-radio");
+const petTypeInput = document.querySelector(".js-pet-type");
+const name = document.querySelector(".js-name").value;
+const phone = document.querySelector(".js-phone").value;
+const pickupLocation = document.querySelector(".js-pickup-location").value;
+const petName = document.querySelector(".js-pet-name").value;
+const email = document.querySelector(".js-email__input").value;
+let petType;
+
+const params = {
+  name,
+  phone,
+  email,
+  pickupLocation,
+  petName,
+  petType
+};
 
 submitButton.addEventListener("click", event => {
   event.preventDefault();
+
   checkTextsOnSubmit();
-  checkEmail(emailInput);
+  checkEmail(emailField);
+  checkPhone(phoneField);
   checkradios();
 
   if (
-    event.preventDefault() &&
     checkTextsOnSubmit() &&
-    checkEmail(emailInput) &&
+    checkEmail(emailField) &&
+    checkPhone(phoneField) &&
     checkradios()
   ) {
-    // make axios call
+    (async () => {
+      try {
+        const response = await axios.post(
+          "https://reqres.in/api/users",
+          params
+        );
+        console.log(response);
+      } catch (error) {
+        console.error(error);
+      }
+    })();
   }
 });
+
+(function getPetType() {
+  petType = petTypeInput.checked ? "Dog" : "Cat";
+})();
 
 function checkTextsOnSubmit() {
   let areAllFull = false;
   textInputs.forEach(textInput => {
     if (textInput.value === "") {
       textInput.classList.add("error");
-      return areAllFull;
+      areAllFull === false;
     }
-    return (areAllFull = true);
+    areAllFull = true;
   });
+  return areAllFull;
 }
 
 function checkEmail(input) {
@@ -44,10 +78,21 @@ function checkradios() {
   let areAllchecked = false;
   radios.forEach(radio => {
     if (radio.checked) {
-      return (areAllchecked = true);
+      areAllchecked = true;
     } else {
-      radio.classList.add("error");
-      return areAllchecked;
+      radio.parentElement.classList.add("error");
+      areAllchecked = false;
     }
   });
+  return areAllchecked;
+}
+
+function checkPhone(input) {
+  const phoneFormat = /^[\+]?[0-9]{5,12}$/;
+  if (input.value.match(phoneFormat)) {
+    return true;
+  } else {
+    input.classList.add("error");
+    return false;
+  }
 }
